aff0e6375d00a62b8ea58a8a70cd7558992f7111
Axel Martin <axel.martin@outlook.com>
2024-03-12 16:32:45 +0100

    add titre pro and second spécialisation presentation to skills page

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -472,6 +472,138 @@ export default function Skills() {
             </div>
           </div>
 
+          <div className={styles.titreProContainer}>
+            <div className={styles.titleContainer}>
+              <h3 className={styles.title}>Titre Professionnel</h3>
+              <div className={styles.titleSeparator}></div>
+            </div>
+            <div className={styles.titreProContentContainer}>
+              <p>
+                Après la validation de mon projet par l’équipe pédagogique
+                d’O’Clock, j’ai pu me présenter au Titre Professionnel dans le
+                cadre d’une session de validation à Paris. <br></br>J’ai dû
+                rédiger un dossier de projet en lien avec le projet d’Apothéose
+                et un dossier professionnel qui m’ont permis de décrire mon
+                expérience professionnelle acquise en cours de formation, en
+                accord avec chacune des compétences du Titre et ainsi, prouver
+                que j’étais capable d’exercer le métier de Développeur Web de
+                façon professionnelle.
+              </p>
+              <p>
+                Le titre professionnel est une certification professionnelle
+                délivrée, au nom de L’Etat, par le ministère chargé de l’emploi.
+                <br></br>
+                O’clock est un centre d’examen agrée par le ministère et
+                respecte un cahier des charges précis quant aux modalités de
+                contrôle continu et présentation des stagiaires aux sessions de
+                validation. <br></br>Le référentiel de certification du Titre
+                Professionnel Développer Web et Web Mobile est le suivant :
+              </p>
+              <div className={styles.moduleContainer}>
+                <p className={styles.moduleTitle}>
+                  Activité n°1 : Développer la partie front-end d’une
+                  application web ou web mobile en intégrant les recommandations
+                  de sécurité
+                </p>
+                <ul className={styles.list}>
+                  <li>Maquetter une application</li>
+                  <li>
+                    Réaliser une interface utilisateur web statique et adaptable
+                  </li>
+                  <li>Développer une interface utilisateur web dynamique</li>
+                </ul>
+              </div>
+              <div className={styles.moduleContainer}>
+                <p className={styles.moduleTitle}>
+                  Activité n°2 : Développer la partie back-end d’une application
+                  web ou web mobile en intégrant les recommandations de sécurité
+                </p>
+                <ul className={styles.list}>
+                  <li>Créer une base de données</li>
+                  <li>Développer les composants d’accès aux données</li>
+                  <li>
+                    Développer la partie back-end d’une application web ou web
+                    mobile
+                  </li>
+                </ul>
+              </div>
+              <p>
+                Un jury composé de professionnels habilités par le ministère
+                chargé de l’emploi s’est alors prononcé sur ma maîtrise de ces
+                compétences en se basant sur les éléments suivants :
+              </p>
+              <div className={styles.moduleContainer}>
+                <ul className={styles.list}>
+                  <li>
+                    Épreuve de synthèse : dossier de présentation de mon travail
+                  </li>
+                  <li>
+                    Dossier professionnel : description des activités réalisées
+                    pendant la formation
+                  </li>
+                  <li>Résultats des évaluations en cours de formation (ECF)</li>
+                  <li>
+                    Entretien avec présentation du projet, questions du jury sur
+                    le projet, questions du jury sur le parcours personnel et
+                    dossier professionnel.
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.secondSpecialisationContainer}>
+            <div className={styles.titleContainer}>
+              <h3 className={styles.title}>Deuxième spécialisation</h3>
+              <div className={styles.titleSeparator}></div>
+            </div>
+            <div className={styles.secondSpecialisationContentContainer}>
+              <p>
+                Après l’obtention de mon Titre Professionnel, ayant accès aux
+                replays des cours et aux fiches techniques de la spécialisation
+                back-end : Data & Api, j’ai décidé de suivre cette formation de
+                façon autonome
+              </p>
+              <div className={styles.moduleContainer}>
+                <p className={styles.moduleTitle}>Module : SQL :</p>
+                <ul className={styles.list}>
+                  <li>Manipulation de données structurées</li>
+                  <li>Systèmes SQL</li>
+                  <li>Procédures stockées</li>
+                  <li>Agrégats</li>
+                  <li>DV (visualisation)</li>
+                </ul>
+              </div>
+              <div className={styles.moduleContainer}>
+                <p className={styles.moduleTitle}>Module : API DATA :</p>
+                <ul className={styles.list}>
+                  <li>Mise à niveau JavaScript/Node</li>
+                  <li>Frameworks NodeJS</li>
+                  <li>Programmation Fonctionnelle</li>
+                  <li>Expressions Régulières </li>
+                </ul>
+              </div>
+              <div className={styles.moduleContainer}>
+                <p className={styles.moduleTitle}>Module : NoSql :</p>
+                <ul className={styles.list}>
+                  <li>Manipulation de données non structurées</li>
+                  <li>Redis, MongoDB</li>
+                  <li>GraphQL</li>
+                  <li>Agrégats</li>
+                </ul>
+              </div>
+              <div className={styles.moduleContainer}>
+                <p className={styles.moduleTitle}>Module : DataScience :</p>
+                <ul className={styles.list}>
+                  <li>Principes de l’intelligence Artificielle</li>
+                  <li>Machine Learning</li>
+                  <li>Modèles prédictifs</li>
+                  <li>Big DATA</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
           {/* <div className={styles.specialisationContainer}>
             <div className={styles.titleContainer}>
               <h3 className={styles.title}>Spécialisation</h3>
@@ -492,237 +624,5 @@ export default function Skills() {
         </section>
       </div>
     </main>
-
-    // <main>
-    //   <div className={styles.skillsContainer}>
-    //     <section className={styles.skillsCardContainer}>
-    //       <div className={styles.skillsCard}>
-    //         <div className={styles.skillsContentContainer}>
-    //           <div className={styles.titleContainer}>
-    //             <h2 className={styles.title}>My Skills</h2>
-    //           </div>
-    //           <div className={styles.skillsGridContainer}>
-    //             {/* HTML --> */}
-    //             <div className={styles.itemContainer}>
-    //               <Image
-    //                 className={styles.itemIcon}
-    //                 src={linkedin}
-    //                 width={40}
-    //                 height={40}
-    //                 alt="Linkedin icon"
-    //               />
-    //               <p className={styles.itemName}>Html</p>
-    //             </div>
-    //             {/* CSS --> */}
-    //             <div className={styles.itemContainer}>
-    //               <Image
-    //                 className={styles.itemIcon}
-    //                 src={linkedin}
-    //                 width={40}
-    //                 height={40}
-    //                 alt="Linkedin icon"
-    //               />
-    //               <p className={styles.itemName}>Css</p>
-    //             </div>
-    //             {/* JavaScript --> */}
-    //             <div className={styles.itemContainer}>
-    //               <Image
-    //                 className={styles.itemIcon}
-    //                 src={linkedin}
-    //                 width={40}
-    //                 height={40}
-    //                 alt="Linkedin icon"
-    //               />
-    //               <p className={styles.itemName}>JavaScript</p>
-    //             </div>
-    //             {/* React --> */}
-    //             <div className={styles.itemContainer}>
-    //               <Image
-    //                 className={styles.itemIcon}
-    //                 src={linkedin}
-    //                 width={40}
-    //                 height={40}
-    //                 alt="Linkedin icon"
-    //               />
-    //               <p className={styles.itemName}>React</p>
-    //             </div>
-    //             {/* NextJs --> */}
-    //             <div className={styles.itemContainer}>
-    //               <Image
-    //                 className={styles.itemIcon}
-    //                 src={linkedin}
-    //                 width={40}
-    //                 height={40}
-    //                 alt="Linkedin icon"
-    //               />
-    //               <p className={styles.itemName}>NextJs</p>
-    //             </div>
-    //             {/* NodeJs --> */}
-    //             <div className={styles.itemContainer}>
-    //               <Image
-    //                 className={styles.itemIcon}
-    //                 src={linkedin}
-    //                 width={40}
-    //                 height={40}
-    //                 alt="Linkedin icon"
-    //               />
-    //               <p className={styles.itemName}>NodeJs</p>
-    //             </div>
-
-    //             {/* TypeScript --> */}
-    //             <div className={styles.itemContainer}>
-    //               <Image
-    //                 className={styles.itemIcon}
-    //                 src={linkedin}
-    //                 width={40}
-    //                 height={40}
-    //                 alt="Linkedin icon"
-    //               />
-    //               <p className={styles.itemName}>Typescript</p>
-    //             </div>
-    //             {/* SQL --> */}
-    //             <div className={styles.itemContainer}>
-    //               <Image
-    //                 className={styles.itemIcon}
-    //                 src={linkedin}
-    //                 width={40}
-    //                 height={40}
-    //                 alt="Linkedin icon"
-    //               />
-    //               <p className={styles.itemName}>SQL</p>
-    //             </div>
-    //             {/* PostgreSQL --> */}
-    //             <div className={styles.itemContainer}>
-    //               <Image
-    //                 className={styles.itemIcon}
-    //                 src={linkedin}
-    //                 width={40}
-    //                 height={40}
-    //                 alt="Linkedin icon"
-    //               />
-    //               <p className={styles.itemName}>PostgreSQL</p>
-    //             </div>
-    //             {/* GIT --> */}
-    //             <div className={styles.itemContainer}>
-    //               <Image
-    //                 className={styles.itemIcon}
-    //                 src={linkedin}
-    //                 width={40}
-    //                 height={40}
-    //                 alt="Linkedin icon"
-    //               />
-    //               <p className={styles.itemName}>GIT</p>
-    //             </div>
-    //             {/* Agile --> */}
-    //             <div className={styles.itemContainer}>
-    //               <Image
-    //                 className={styles.itemIcon}
-    //                 src={linkedin}
-    //                 width={40}
-    //                 height={40}
-    //                 alt="Linkedin icon"
-    //               />
-    //               <p className={styles.itemName}>Agile</p>
-    //             </div>
-    //             {/* Figma --> */}
-    //             <div className={styles.itemContainer}>
-    //               <Image
-    //                 className={styles.itemIcon}
-    //                 src={linkedin}
-    //                 width={40}
-    //                 height={40}
-    //                 alt="Linkedin icon"
-    //               />
-    //               <p className={styles.itemName}>Figma</p>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </section>
-
-    //     <section className={styles.formationContainer}>
-    //       <div className={styles.formationPresentationContainer}>
-    //         <div className={styles.titleContainer}>
-    //           <h2 className={styles.title}>My formation</h2>
-    //         </div>
-    //         <div className={styles.formationPresentationContentContainer}>
-    //           <p>
-    //             J’ai choisi de faire ma reconversion professionnelle à l’école
-    //             O’Clock en suivant la formation Développeur Web Fullstack
-    //             JavaScript.
-    //           </p>
-    //           <p>
-    //             Cette formation intensive, d’une durée de 6 mois, proposée sur
-    //             le format téléprésentiel prépare au Titre Professionnel
-    //             Développeur Web et Web Mobile. <br></br>Il s’agit d’un diplôme
-    //             de niveau 5 (nomenclature Europe) correspondant au niveau III et
-    //             inscrit au Répertoire National de la Certification
-    //             Professionnelle (RNCP). <br></br>Le titre Professionnel est une
-    //             certification professionnelle délivrée au nom de l’Etat par le
-    //             ministère chargé de l’emploi.
-    //           </p>
-    //           <p>
-    //             L’objectif de cette formation est de fournir des compétences clé
-    //             aux apprenants, leur permettant d’être directement employables à
-    //             la fin du cursus. Comme par exemple :
-    //           </p>
-    //           <ul className={styles.list}>
-    //             <li>Intégrer des pages web en HTML et CSS</li>
-    //             <li>Développer des fonctionnalités front-end en JavaScript</li>
-    //             <li>Concevoir des interfaces utilisateur</li>
-    //             <li>Concevoir, implémenter et utiliser des bases de données</li>
-    //             <li>
-    //               Développer une application serveur avec Node.js et Express
-    //             </li>
-    //             <li>
-    //               Travailler en groupe sur un projet dans des conditions
-    //               professionnelles.
-    //             </li>
-    //           </ul>
-    //           <p>
-    //             La formation a lieu tous les jours ouvrés de 9 h à 17 h. Après
-    //             les cours, nous devions réaliser au minimum deux heures de
-    //             travaux dirigés sous forme de challenge, mini-projet,
-    //             d’exercices, de tutoriels, de sessions de corrections de bug, de
-    //             veille professionnel, de lecture de documentation techniques etc
-    //             … <br></br>Ces travaux dirigés permettent d’approfondir les
-    //             sujets vus en cours durant la journée, de découvrir de nouvelles
-    //             notions (pédagogie inversée) et d’apprendre à collaborer sur des
-    //             projets en équipe. <br></br> L’évaluation des compétences est
-    //             assurée durant la totalité de la formation grâce à un contrôle
-    //             continu des travaux dirigés, d’évaluations en cours de
-    //             formation, d’entretiens individuels et de conseils de classe
-    //             interne entre chaque saison. Puis lors du contrôle final qui est
-    //             la session de validation du Titre Professionnel.
-    //           </p>
-    //           <p>
-    //             La formation est structurée en 4 phases distinctes : d’abord, le
-    //             socle, suivit par un mois de spécialisation. Ensuite, vient le
-    //             projet d’apothéose et enfin, le dernier mois est consacré à la
-    //             préparation de la soutenance finale
-    //           </p>
-    //         </div>
-    //       </div>
-
-    //       <div className={styles.socleContainer}>
-    //         <div className={styles.titleContainer}>
-    //           <h2 className={styles.title}>Le socle</h2>
-    //           <div className={styles.titleSeparator}></div>
-    //         </div>
-    //         <div className={styles.socleContentContainer}>
-    //           <p>
-    //             Le Socle est le nom donné à la première phase de la formation.
-    //             Nous sommes formés aux langages et aux technologies qui
-    //             constitueront une base de compétences. On apprend à utiliser les
-    //             langages HTML, CSS, JavaScript, SQL et à utiliser Node.js à
-    //             travers de multiples activités pédagogiques. Nous sommes
-    //             également évalués tout au long du parcours, sur la base des
-    //             compétences du référentiel du Titre Professionnel.
-    //           </p>
-    //         </div>
-    //       </div>
-    //     </section>
-    //   </div>
-    // </main>
   );
 }

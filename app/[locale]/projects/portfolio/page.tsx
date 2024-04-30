2fbce30c68b4b7cf36a99bb391142fd86a7654c6
Axel Martin <axel.martin@outlook.com>
2024-04-30 20:58:36 +0200

    restructure portfolio project page

--- app/[locale]/projects/portfolio/page.tsx
+++ app/[locale]/projects/portfolio/page.tsx
@@ -9,67 +9,69 @@ export default function Portfolio() {
   return (
     <main>
       <div className={styles.projectContainer}>
-        <section className={styles.presentationSection}>
+        <section className={styles.projectImageSection}>
           <div className={styles.projectImageContainer}>
             <Image
+              className={styles.img}
               src="/images/projects/portfolio/portfoliologo.png"
               alt={t('presentationImg-alt')}
               fill
             />
           </div>
+        </section>
 
-          <div className={styles.projectPresentationContainer}>
-            <div className={styles.projectHeader}>
-              <div className={styles.titlesContainer}>
-                <div className={styles.titlesContentContainer}>
-                  <h1 className={styles.projectName}>Portfolio v1</h1>
-                  <div className={styles.subTitleContainer}>
-                    <div className={styles.separator}></div>
-                    <h2 className={styles.subTitle}>{t('sub-title')}</h2>
-                    <div className={styles.separator}></div>
-                  </div>
+        <section className={styles.presentationSection}>
+          <div className={styles.projectHeader}>
+            <div className={styles.titlesContainer}>
+              <div className={styles.titlesContentContainer}>
+                <h1 className={styles.projectName}>Portfolio v1</h1>
+                <div className={styles.subTitleContainer}>
+                  <div className={styles.separator}></div>
+                  <h2 className={styles.subTitle}>{t('sub-title')}</h2>
+                  <div className={styles.separator}></div>
                 </div>
               </div>
-              <div className={styles.skillsContainer}>
-                <p className={styles.skills}>Html</p>
-                <p className={styles.skills}>Scss</p>
-                <p className={styles.skills}>Next.js</p>
-                <p className={styles.skills}>React</p>
-                <p className={styles.skills}>TypeScript</p>
-                <p className={styles.skills}>EsLint</p>
-                <p className={styles.skills}>Next-Intl</p>
-                <p className={styles.skills}>Git</p>
-                <p className={styles.skills}>Figma</p>
-              </div>
             </div>
 
-            <div className={styles.projectDescription}>
-              <p>
-                {t('description-part1-line1')} <br></br>
-                {t('description-part1-line2')} <br></br>
-                {t('description-part1-line3')} <br></br>
-                {t('description-part1-line4')}
-              </p>
-              <p>
-                {t('description-part2-line1')} <br></br>
-                {t('description-part2-line2')} <br></br>
-                {t('description-part2-line3')} <br></br>
-                {t('description-part2-line4')} <br></br>
-                {t('description-part2-line5')}
-              </p>
+            <div className={styles.skillsContainer}>
+              <p className={styles.skills}>Html</p>
+              <p className={styles.skills}>Scss</p>
+              <p className={styles.skills}>Next.js</p>
+              <p className={styles.skills}>TypeScript</p>
+              <p className={styles.skills}>Node.js</p>
+              <p className={styles.skills}>Express</p>
+              <p className={styles.skills}>PostgreSQL</p>
+              <p className={styles.skills}>Agile</p>
+              <p className={styles.skills}>Git</p>
             </div>
-
-            <a
-              href="https://github.com/Axel-Martin7/portfolio"
-              className={styles.buttonContainer}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button className={styles.btn} aria-label={t('button-arialabel')}>
-                Github
-              </button>
-            </a>
           </div>
+
+          <div className={styles.projectDescription}>
+            <p>
+              {t('description-part1-line1')} <br></br>
+              {t('description-part1-line2')} <br></br>
+              {t('description-part1-line3')} <br></br>
+              {t('description-part1-line4')}
+            </p>
+            <p>
+              {t('description-part2-line1')} <br></br>
+              {t('description-part2-line2')} <br></br>
+              {t('description-part2-line3')} <br></br>
+              {t('description-part2-line4')} <br></br>
+              {t('description-part2-line5')}
+            </p>
+          </div>
+
+          <a
+            href="https://github.com/Axel-Martin7/portfolio"
+            className={styles.buttonContainer}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className={styles.btn} aria-label={t('button-arialabel')}>
+              Repos Github
+            </button>
+          </a>
         </section>
 
         <section className={styles.otherProjectsSection}>

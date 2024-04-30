46d0660de2a2149d4ed5a70136e7e25eb1d7f1c0
Axel Martin <axel.martin@outlook.com>
2024-04-30 21:11:46 +0200

    restructure okanban project page

--- app/[locale]/projects/okanban/page.tsx
+++ app/[locale]/projects/okanban/page.tsx
@@ -9,68 +9,70 @@ export default function Okanban() {
   return (
     <main>
       <div className={styles.projectContainer}>
-        <section className={styles.presentationSection}>
+        <section className={styles.projectImageSection}>
           <div className={styles.projectImageContainer}>
             <Image
+              className={styles.img}
               src="/images/projects/okanban/okanbandesktop.png"
               alt={t('presentationImg-alt')}
               fill
             />
           </div>
+        </section>
 
-          <div className={styles.projectPresentationContainer}>
-            <div className={styles.projectHeader}>
-              <div className={styles.titlesContainer}>
-                <div className={styles.titlesContentContainer}>
-                  <h1 className={styles.projectName}>Okanban</h1>
-                  <div className={styles.subTitleContainer}>
-                    <div className={styles.separator}></div>
-                    <h2 className={styles.subTitle}>{t('sub-title')}</h2>
-                    <div className={styles.separator}></div>
-                  </div>
+        <section className={styles.presentationSection}>
+          <div className={styles.projectHeader}>
+            <div className={styles.titlesContainer}>
+              <div className={styles.titlesContentContainer}>
+                <h1 className={styles.projectName}>Okanban</h1>
+                <div className={styles.subTitleContainer}>
+                  <div className={styles.separator}></div>
+                  <h2 className={styles.subTitle}>{t('sub-title')}</h2>
+                  <div className={styles.separator}></div>
                 </div>
               </div>
-              <div className={styles.skillsContainer}>
-                <p className={styles.skills}>Html</p>
-                <p className={styles.skills}>Css</p>
-                <p className={styles.skills}>JavaScript</p>
-                <p className={styles.skills}>Node.js</p>
-                <p className={styles.skills}>Express</p>
-                <p className={styles.skills}>PostgreSQL</p>
-                <p className={styles.skills}>Sequelize</p>
-              </div>
             </div>
 
-            <div className={styles.projectDescription}>
-              <p>
-                {t('description-part1-line1')} <br></br>
-                {t('description-part1-line2')} <br></br>
-                {t('description-part1-line3')}
-                <br></br>
-                {t('description-part1-line4')} <br></br>
-                {t('description-part1-line5')}
-              </p>
-              <p>
-                {t('description-part2-line1')} <br></br>
-                {t('description-part2-line2')} <br></br>
-                {t('description-part2-line3')} <br></br>
-                {t('description-part2-line4')} <br></br>
-                {t('description-part2-line5')} <br></br>
-                {t('description-part2-line6')} <br></br>
-              </p>
+            <div className={styles.skillsContainer}>
+              <p className={styles.skills}>Html</p>
+              <p className={styles.skills}>Css</p>
+              <p className={styles.skills}>JavaScript</p>
+              <p className={styles.skills}>Node.js</p>
+              <p className={styles.skills}>Express</p>
+              <p className={styles.skills}>PostgreSQL</p>
+              <p className={styles.skills}>Sequelize</p>
             </div>
-
-            <a
-              href="https://github.com/Axel-Martin7/okanban-backend"
-              className={styles.buttonContainer}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button className={styles.btn} aria-label={t('button-arialabel')}>
-                Github back
-              </button>
-            </a>
           </div>
+
+          <div className={styles.projectDescription}>
+            <p>
+              {t('description-part1-line1')} <br></br>
+              {t('description-part1-line2')} <br></br>
+              {t('description-part1-line3')}
+              <br></br>
+              {t('description-part1-line4')} <br></br>
+              {t('description-part1-line5')}
+            </p>
+            <p>
+              {t('description-part2-line1')} <br></br>
+              {t('description-part2-line2')} <br></br>
+              {t('description-part2-line3')} <br></br>
+              {t('description-part2-line4')} <br></br>
+              {t('description-part2-line5')} <br></br>
+              {t('description-part2-line6')} <br></br>
+            </p>
+          </div>
+
+          <a
+            href="https://github.com/Axel-Martin7/okanban-backend"
+            className={styles.buttonContainer}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className={styles.btn} aria-label={t('button-arialabel')}>
+              Github back-end
+            </button>
+          </a>
         </section>
 
         <section className={styles.imagesSection}>
